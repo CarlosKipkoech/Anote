a1d3495ce9ad01ec7f8b16a2c46028cff4369d1e
CarlosKipkoech <carlos598798@gmail.com>
2023-10-09 15:18:18 +0300

    add merger

--- app/src/main/java/com/codezilla/cleanarchitecturenoteapp/feature_note/presentation/MainActivity.kt
+++ app/src/main/java/com/codezilla/cleanarchitecturenoteapp/feature_note/presentation/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.plcoding.cleanarchitecturenoteapp
+package com.codezilla.cleanarchitecturenoteapp.feature_note.presentation
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
